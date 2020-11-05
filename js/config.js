a394ddfdae4832677f1a62f58c79d6d963aaa7b0
GeorgeXCV <georgeashton95@gmail.com>
2020-11-05 19:23:08 +0000

    Dummy Config

---
+++ js/config.js
@@ -0,0 +1,4 @@
+module.exports.config = {
+    apiKeyOpenXBL: "YOUR_API_KEY", // https://xbl.io/ (Better API, higher rate limits)
+    apiKeyXboxAPI: "YOUR_API_KEY" // https://xapi.us/ (Low rate limits, only use when have to)
+}
